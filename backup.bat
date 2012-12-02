7ba482ff935dcc9b8b54ed3f80c4cc47f9a9f610
root <root@10.0.2.15>
2012-12-02 20:39:17 +0800

    构建新的 ENV “trac”，用于 PortableTrac 的演示

--- backup.bat
+++ backup.bat
@@ -4,4 +4,5 @@ SETLOCAL
 cd /d %~dp0%
 set SITE_BASE=%cd%
 call ..\PortableTrac-git\trac-backup.cmd main
+call ..\PortableTrac-git\trac-backup.cmd trac
 ENDLOCAL
---
+++ restore.bat
@@ -0,0 +1,9 @@
+@echo off
+
+SETLOCAL
+cd /d %~dp0%
+set SITE_BASE=%cd%
+call ..\PortableTrac-git\bin\init-env.bat
+call ..\PortableTrac-git\bin\do_restore.bat main
+call ..\PortableTrac-git\bin\do_restore.bat trac
+ENDLOCAL
