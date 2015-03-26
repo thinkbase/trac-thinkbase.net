7cc27b5dffa247359002cb1a7851048ef174c631
zzj <zzj@zzj-Inspiron-7420>
2015-03-26 11:22:28 +0800

    Add restore script for docker container "PortableTrac" - https://github.com/thinkbase/Dockerfiles/tree/master/09-Apps/01-PortableTrac

---
+++ restore.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+set -o nounset
+set -o errexit
+set -x
+# This is the restore shell script only for docker container "PortableTrac".
+#   - https://github.com/thinkbase/Dockerfiles/tree/master/09-Apps/01-PortableTrac
+
+export SITE_BASE=/data
+/home/u01/github/PortableTrac/bin/do_restore.sh main
+/home/u01/github/PortableTrac/bin/do_restore.sh trac
