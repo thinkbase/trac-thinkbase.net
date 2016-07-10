32eeae287c694debb3ef7160250b894b7f0ff9b3
thinkbase <thinkbase@users.noreply.github.com>
2016-07-10 13:50:54 +0800

    Prepare the trac backup script

--- etc/shells/trac-backup.sh
+++ etc/shells/trac-backup.sh
@@ -19,19 +19,23 @@ source /private/git-tokens.conf
 rm -rfv /data/trac/backup/.old
 
 cd /data/trac
+# Before backup, pull the newest remote repo first
+git pull
+# Run trac backup, check git folder status
 ./backup.sh
 git status
 
+# Force update git configurations every time before commit
 git config user.name "oper1"
 git config user.email "thinkbase-oper1@users.noreply.github.com"
 git config push.default simple
 
+# Add changed files then commit
 git add --all .
-
 set +o errexit  # commit should return non-zero if nothing could commit
 git commit -m "Daily backup `date "+%Y%m%d-%H%M%S"`"
 set -o errexit
 
+# Push to remote repo
 git remote -v
-
 git push https://${TOKEN_TRAC_THINKBASE_NET}@github.com/thinkbase/trac-thinkbase.net.git
