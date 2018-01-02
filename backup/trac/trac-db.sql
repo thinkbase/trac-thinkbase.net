a34cd7f6b8d7432509b54467ec13af5063cfdcc7
oper1 <thinkbase-oper1@users.noreply.github.com>
2018-01-02 16:01:21 +0800

    Daily backup 20180102-160121

--- backup/trac/trac-db.sql
+++ backup/trac/trac-db.sql
@@ -3455,6 +3455,7 @@ INSERT INTO "session" VALUES('fc6997bd54bc7e0d4ecdfc6c',0,1514609783);
 INSERT INTO "session" VALUES('c47c98f7c459f65bae771951',0,1514649590);
 INSERT INTO "session" VALUES('5cacd307624e1d7b1ea3c303',0,1514782232);
 INSERT INTO "session" VALUES('394279e9b3c1e6952550ea39',0,1514821187);
+INSERT INTO "session" VALUES('877e6f78123a9389d0fbd84d',0,1514869197);
 CREATE TABLE session_attribute (
     sid text,
     authenticated integer,
@@ -14206,6 +14207,10 @@ INSERT INTO "session_attribute" VALUES('394279e9b3c1e6952550ea39',0,'query_time'
 INSERT INTO "session_attribute" VALUES('394279e9b3c1e6952550ea39',0,'query_constraints','[{''status'': [u''!closed'']}]');
 INSERT INTO "session_attribute" VALUES('394279e9b3c1e6952550ea39',0,'query_href','/trac/query?status=!closed&order=id');
 INSERT INTO "session_attribute" VALUES('394279e9b3c1e6952550ea39',0,'query_tickets','');
+INSERT INTO "session_attribute" VALUES('877e6f78123a9389d0fbd84d',0,'query_time','1514869196');
+INSERT INTO "session_attribute" VALUES('877e6f78123a9389d0fbd84d',0,'query_constraints','[{u''status'': [u''!closed'']}]');
+INSERT INTO "session_attribute" VALUES('877e6f78123a9389d0fbd84d',0,'query_href','/trac/query?status=!closed&order=priority');
+INSERT INTO "session_attribute" VALUES('877e6f78123a9389d0fbd84d',0,'query_tickets','');
 CREATE TABLE attachment (
     type text,
     id text,
