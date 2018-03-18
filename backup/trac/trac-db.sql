4a8728ce321b1e124643aada1800f85e0ae0c0df
oper1 <thinkbase-oper1@users.noreply.github.com>
2018-03-18 16:00:38 +0800

    Daily backup 20180318-160037

--- backup/trac/trac-db.sql
+++ backup/trac/trac-db.sql
@@ -4523,6 +4523,9 @@ INSERT INTO "session" VALUES('a42453ae2c98fec127bbc151',0,1521098228);
 INSERT INTO "session" VALUES('b64a7d7018d506b4dea4473a',0,1521098240);
 INSERT INTO "session" VALUES('80c6b37330773593eadd5012',0,1521145177);
 INSERT INTO "session" VALUES('9b821e289479022631a98bb4',0,1521312253);
+INSERT INTO "session" VALUES('40bf4d814f1dbd445677a1ff',0,1521326771);
+INSERT INTO "session" VALUES('865eaea5af4684ecc34e55d5',0,1521326790);
+INSERT INTO "session" VALUES('8fc8a7979c7f6d7bef713e4f',0,1521326812);
 CREATE TABLE session_attribute (
     sid text,
     authenticated integer,
@@ -18950,6 +18953,14 @@ INSERT INTO "session_attribute" VALUES('80c6b37330773593eadd5012',0,'query_href'
 INSERT INTO "session_attribute" VALUES('80c6b37330773593eadd5012',0,'query_tickets','');
 INSERT INTO "session_attribute" VALUES('9b821e289479022631a98bb4',0,'query_href','/trac/report/3?asc=1&page=1');
 INSERT INTO "session_attribute" VALUES('9b821e289479022631a98bb4',0,'query_tickets','');
+INSERT INTO "session_attribute" VALUES('40bf4d814f1dbd445677a1ff',0,'query_time','1521326769');
+INSERT INTO "session_attribute" VALUES('40bf4d814f1dbd445677a1ff',0,'query_constraints','[{u''status'': [u''!closed'']}]');
+INSERT INTO "session_attribute" VALUES('40bf4d814f1dbd445677a1ff',0,'query_href','/trac/query?status=!closed&order=priority');
+INSERT INTO "session_attribute" VALUES('40bf4d814f1dbd445677a1ff',0,'query_tickets','');
+INSERT INTO "session_attribute" VALUES('865eaea5af4684ecc34e55d5',0,'query_href','/trac/report/6?asc=1&USER=anonymous&page=1');
+INSERT INTO "session_attribute" VALUES('865eaea5af4684ecc34e55d5',0,'query_tickets','');
+INSERT INTO "session_attribute" VALUES('8fc8a7979c7f6d7bef713e4f',0,'query_href','/trac/report/7?asc=1&USER=anonymous&page=1');
+INSERT INTO "session_attribute" VALUES('8fc8a7979c7f6d7bef713e4f',0,'query_tickets','');
 CREATE TABLE attachment (
     type text,
     id text,
