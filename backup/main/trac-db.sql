d92eebdc6623351b2774db988720d8cf9d72a167
oper1 <thinkbase-oper1@users.noreply.github.com>
2019-04-13 16:00:54 +0800

    Daily backup 20190413-160054

--- backup/main/trac-db.sql
+++ backup/main/trac-db.sql
@@ -1918,6 +1918,8 @@ INSERT INTO "session" VALUES('c59ec2b19976f4327c038a92',0,1554949673);
 INSERT INTO "session" VALUES('6cbe1e0d1ef31870a4f64b20',0,1554949689);
 INSERT INTO "session" VALUES('4255b7b82fa976c241ee0d10',0,1554949711);
 INSERT INTO "session" VALUES('d9fbfd1552bf7c86f4c0d9f6',0,1554982230);
+INSERT INTO "session" VALUES('fc25afd8022ee062c0b880b2',0,1555113731);
+INSERT INTO "session" VALUES('c004bc0874548c8d9cafe1fb',0,1555137679);
 CREATE TABLE session_attribute (
     sid text,
     authenticated integer,
@@ -5670,6 +5672,10 @@ INSERT INTO "session_attribute" VALUES('4255b7b82fa976c241ee0d10',0,'timeline.la
 INSERT INTO "session_attribute" VALUES('4255b7b82fa976c241ee0d10',0,'timeline.nextlastvisit','0');
 INSERT INTO "session_attribute" VALUES('d9fbfd1552bf7c86f4c0d9f6',0,'timeline.lastvisit','1554969641000000');
 INSERT INTO "session_attribute" VALUES('d9fbfd1552bf7c86f4c0d9f6',0,'timeline.nextlastvisit','0');
+INSERT INTO "session_attribute" VALUES('fc25afd8022ee062c0b880b2',0,'timeline.lastvisit','1555099237000000');
+INSERT INTO "session_attribute" VALUES('fc25afd8022ee062c0b880b2',0,'timeline.nextlastvisit','0');
+INSERT INTO "session_attribute" VALUES('c004bc0874548c8d9cafe1fb',0,'timeline.lastvisit','1555099237000000');
+INSERT INTO "session_attribute" VALUES('c004bc0874548c8d9cafe1fb',0,'timeline.nextlastvisit','0');
 CREATE TABLE attachment (
     type text,
     id text,
